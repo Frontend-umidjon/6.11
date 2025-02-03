eceb376b2ea1dc72286fb5a1890eebb133c1207f
Frontend-Umidjon <umidjonkengashev6@gamil.com>
2025-02-03 08:38:36 +0500

    uy ishi

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Hero from './Hero'
+import Hero from '../../components/hero/Hero'
 import Header from '@/components/header/Header'
 import Footer from '@/components/footer/Footer'
-import HeaderContact from './HeaderContact'
-import EditorHome from './EditorHome'
-import ProductsHome from './ProductsHome'
-import SummerHome from './SummerHome'
-import FeaturedHome from './FeaturedHome'
+import HeaderContact from '../../components/headerContact/HeaderContact'
+import EditorHome from '../../components/editorHome/EditorHome'
+import ProductsHome from '../../components/productsHome/ProductsHome'
+import SummerHome from '../../components/summerHome/SummerHome'
+import FeaturedHome from '../../components/feauturedHome/FeaturedHome'
 
 export const Home = () => {
     return (
